bb2a8e4ccfaca43662f5b9de8f8afdb2ef4095b2
smpollard0 <spollard301@gmail.com>
2024-02-11 12:17:36 -0800

    add camera rotation using the mouse closes #11

--- CSCI Capstone/Assets/Scripts/CameraMovement.cs
+++ CSCI Capstone/Assets/Scripts/CameraMovement.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 1000f;
     float xRotation = 0f;
+    float yRotation = 0f;
+
     // Update is called once per frame
     void Update()
     {
+        // GetMouseButton(0) = LMB
+        // GetMouseButton(1) = RMB
         // if the right mouse button is held down
         if (Input.GetMouseButton(1)){
-            // what the fuck does Time.deltaTime do?
+
+            // capture mouse movement independent of framerate by multiplying the mouse movement by Time.deltaTime
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+            // when the user moves their mouse to the left, the camera rotates right and vice versa
             xRotation -= mouseY;
+            yRotation += mouseX;
+            // clamp the rotation so the user doesn't just keep rotating upward or downward
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            
             // use the components of the turn vector to rotate the camera
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f); 
+
         }
     }
 }
