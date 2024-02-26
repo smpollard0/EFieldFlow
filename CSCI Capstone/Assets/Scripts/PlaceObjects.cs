650602534d67e50c0ce23fabfe9f3d53eca3b890
smpollard0 <spollard301@gmail.com>
2024-02-25 16:05:25 -0800

    place spheres in scene when they collide with a surface or at a fixed distance from the camera

--- CSCI Capstone/Assets/Scripts/PlaceObjects.cs
+++ CSCI Capstone/Assets/Scripts/PlaceObjects.cs
@@ -5,61 +5,66 @@ using UnityEngine;
 public class PlaceObjects : MonoBehaviour{
 
     private Camera mainCamera; // Reference to the main camera
+    private float distanceFromCamera = 10f;
     
     bool buttonIsPressed = false;
 
     void Awake(){
-        // create reference to main camera
+        // reference the camera
         mainCamera = Camera.main;
     }
 
     void Update() {
         // press Q button to play a game object
         if (Input.GetKey(KeyCode.Q) && !buttonIsPressed){
-            // Cast a ray from the camera through the mouse cursor position
+            // create gameobject to place
+            GameObject objectToPlace;
+            
+            // cast a ray from the camera through the mouse cursor position
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)){
-                // create gameobject to place
-                GameObject objectToPlace;
-                // switch statement to check what kind of object to create based on what is selected in the toolbar
-                switch (UiController.selectedItemIndex){
-                    // point charges
-                    case 0:
-                        Debug.Log("Point Charge Selected");
-                        // temporarily place spheres; come back later and make these prefabs 
-                        objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        // Object placement when the ray intersects with something in the scene
+            // switch statement to check what kind of object to create based on what is selected in the toolbar
+            switch (UiController.selectedItemIndex){
+                // point charges
+                case 0:
+                    // temporarily place spheres; come back later and make these prefabs 
+                    objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    if (Physics.Raycast(ray, out hit)){
+                        //  when the ray intersects with something in the scene, place object
                         SphereCollider sphereCollider = objectToPlace.GetComponent<SphereCollider>();
-                        if (sphereCollider != null){
+                        if (sphereCollider != null){ // should probably have error checking if this is null for some reason
                             float sphereRadius = sphereCollider.radius;
                             Vector3 surfaceOffset = hit.normal * sphereRadius;
-                            // need to cast a ray from the camera to the position of the mouse to place object at mouse location
+                            // set position of object to where the ray hits a collider plus a radius offset so the sphere isn't inside the collided object
                             objectToPlace.transform.position = hit.point + surfaceOffset;
                         }
-                        buttonIsPressed = true;
-                        break;
+                    }
+                    else {
+                        // when the ray doesn't intersect with anything in the scene, place the object a fixed distance from the camera
+                        objectToPlace.transform.position = ray.origin + ray.direction * distanceFromCamera;
+                    }
+                    buttonIsPressed = true;
+                    break;
 
-                    // for the time being, this is commented out because I don't have proper parameters for line or volume charges
-                    // line charges
-                    // case 1:
-                    //     Debug.Log("Line Charge Selected");
-                    //     objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                    //     objectToPlace.transform.position = new Vector3(0, 3f, 0);
-                    //     buttonIsPressed = true;
-                    //     break;
-                    // // volume charges
-                    // case 2:
-                    //     Debug.Log("Volume Charge Selected");
-                    //     objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    //     objectToPlace.transform.position = new Vector3(0, 3f, 0);
-                    //     buttonIsPressed = true;
-                    //     break;
-                    default:
-                        // this should never happen
-                        break;
-                }// switch
-            }
+                // for the time being, this is commented out because I don't have proper parameters for line or volume charges
+                // line charges
+                // case 1:
+                //     Debug.Log("Line Charge Selected");
+                //     objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                //     objectToPlace.transform.position = new Vector3(0, 3f, 0);
+                //     buttonIsPressed = true;
+                //     break;
+                // // volume charges
+                // case 2:
+                //     Debug.Log("Volume Charge Selected");
+                //     objectToPlace = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //     objectToPlace.transform.position = new Vector3(0, 3f, 0);
+                //     buttonIsPressed = true;
+                //     break;
+                default:
+                    // this should never happen
+                    break;
+            }// switch
         }
         if (Input.GetKeyUp(KeyCode.Q)){
             // Reset the flag when the button is released
