8afc6214f09505204b7d763b7c36a6f5f7ee3b64
smpollard0 <spollard301@gmail.com>
2024-04-28 14:38:49 -0700

    implement UI to show efield

--- CSCI Capstone/Assets/Resources/Scripts/ElectricFieldCalculator.cs
+++ CSCI Capstone/Assets/Resources/Scripts/ElectricFieldCalculator.cs
@@ -1,23 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ElectricFieldCalculator : MonoBehaviour {
+    public GameObject EFieldValuesMenuUI;
 
     private const double permittivity = 8.854187817e-12; // Permittivity of free space
+    private TMP_Text outputBox;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.LeftShift)){
-            List<Vector3> fields = CalculateElectricFieldAtPoint();
+    [System.Serializable]
+    public class ElectricFieldResult {
+        private Vector3 electricField;
+        private int testUOID;
 
-            foreach (Vector3 field in fields) {
-                Debug.Log(field);
-            }
+        public Vector3 ElectricField {
+            get { return electricField; }
+            set { electricField = value; }
+        }
+
+        public int TestUOID {
+            get { return testUOID; }
+            set { testUOID = value; }
         }
     }
 
-    public static List<Vector3> CalculateElectricFieldAtPoint() {
-        List<Vector3> electricFields = new List<Vector3>();
+    void Start() {
+        // Initialize outputBox in Start method
+        outputBox = EFieldValuesMenuUI.transform.Find("ResultsBox").GetComponent<TMP_Text>();
+    }
+
+    void Update() {
+
+        // Hide the efield menu if it's active
+        if (EFieldValuesMenuUI.activeSelf && PauseMEnu.GameIsPaused){
+            EFieldValuesMenuUI.SetActive(false);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !PauseMEnu.GameIsPaused){
+            outputBox.text = "";
+            List<ElectricFieldResult> fields = CalculateElectricFieldAtPoint();
+
+            foreach (ElectricFieldResult field in fields) {
+                outputBox.text += field.TestUOID.ToString() + ": ";
+                outputBox.text += field.ElectricField.ToString() + " N/C" + "\n";
+            }
+            EFieldValuesMenuUI.SetActive(true);
+        }
+    }
+
+    public static List<ElectricFieldResult> CalculateElectricFieldAtPoint() {
+        List<ElectricFieldResult> electricFields = new List<ElectricFieldResult>();
 
         List<PointCharge> testCharges = new List<PointCharge>();
         // Find all test charges
@@ -34,34 +68,12 @@ public class ElectricFieldCalculator : MonoBehaviour {
             return electricFields;
         }
 
-        // Iterate through all point charges in the scene
-        // foreach (PointCharge pointCharge in FindObjectsOfType<PointCharge>()) {
-        //     if (pointCharge.ChargeValue != 0) {
-        //         // Iterate through all test charges
-        //         foreach (PointCharge testCharge in testCharges) {
-        //             // Calculate electric field contribution for each test charge
-        //             Vector3 dif = testCharge.transform.position - pointCharge.transform.position;
-        //             double separationR = dif.magnitude;
-
-        //             double eFieldX = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.x) / Mathf.Pow((float)separationR, 3);
-        //             double eFieldY = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.y) / Mathf.Pow((float)separationR, 3);
-        //             double eFieldZ = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.z) / Mathf.Pow((float)separationR, 3);
-
-        //             eFieldX *= (1 / (4 * Mathf.PI * permittivity));
-        //             eFieldY *= (1 / (4 * Mathf.PI * permittivity));
-        //             eFieldZ *= (1 / (4 * Mathf.PI * permittivity));
-
-        //             electricFields.Add(new Vector3((float)eFieldX, (float)eFieldY, (float)eFieldZ));
-        //         }
-        //     }
-        // }
-
         // Iterate over the test charges
         // Iterate over all other charges
         // Add other charges' contributions at a particular test charge
         // Create vector3 and add it to the electricFields vector
-
         foreach (PointCharge testCharge in testCharges){
+            ElectricFieldResult result = new ElectricFieldResult();
             double eFieldX = 0.0;
             double eFieldY = 0.0;
             double eFieldZ = 0.0;
@@ -80,9 +92,17 @@ public class ElectricFieldCalculator : MonoBehaviour {
             eFieldY *= (1 / (4 * Mathf.PI * permittivity));
             eFieldZ *= (1 / (4 * Mathf.PI * permittivity));
 
-            electricFields.Add(new Vector3((float)eFieldX, (float)eFieldY, (float)eFieldZ));
+            result.ElectricField = new Vector3((float)eFieldX, (float)eFieldY, (float)eFieldZ);
+            result.TestUOID = testCharge.UOID;
+            electricFields.Add(result);
         }
 
+
+
         return electricFields;
+    }
+
+    public void CloseMenu(){
+        EFieldValuesMenuUI.SetActive(false);
     }
 }
