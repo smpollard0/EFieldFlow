57a43ed1a978b64d61c4cfcdd6ad3b7ac153d766
smpollard0 <spollard301@gmail.com>
2024-05-01 11:17:54 -0700

    create visual vector field

--- CSCI Capstone/Assets/Resources/Scripts/ElectricFieldCalculator.cs
+++ CSCI Capstone/Assets/Resources/Scripts/ElectricFieldCalculator.cs
@@ -11,7 +11,12 @@ public class ElectricFieldCalculator : MonoBehaviour {
     private const double permittivity = 8.854187817e-12; // Permittivity of free space
     private TMP_Text outputBox;
     private Toggle toggleVectorField;
-    private GameObject boundingBoxCube;
+    private Vector3 minBoundingBox;
+    private Vector3 maxBoundingBox;
+    private float fieldDensity = 1.0f;
+
+    private List<GameObject> instantiatedVectors = new List<GameObject>();
+
 
     [System.Serializable]
     public class ElectricFieldResult {
@@ -51,9 +56,6 @@ public class ElectricFieldCalculator : MonoBehaviour {
             }
             EFieldValuesMenuUI.SetActive(true);
         }
-
-
-
     }
 
     public static List<ElectricFieldResult> CalculateElectricFieldAtPoint() {
@@ -106,39 +108,117 @@ public class ElectricFieldCalculator : MonoBehaviour {
         return electricFields;
     }
 
+    /*
+    This function is slightly different than the one above. For the time being, I opted to create
+    a new function that is used for calculating the individual vectors within some bounding box
+    region so I don't risk breaking the above function for the normal E-Field vector calculation.
+    */
+    public Vector3 CalculateElectricFieldAtPoint(Vector3 position){
+        Vector3 electricField = Vector3.zero;
+
+        foreach (PointCharge pointCharge in FindObjectsOfType<PointCharge>()) {
+            Vector3 dif = position - pointCharge.transform.position;
+            double separationR = dif.magnitude;
+
+            double eFieldX = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.x) / Mathf.Pow((float)separationR, 3);
+            double eFieldY = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.y) / Mathf.Pow((float)separationR, 3);
+            double eFieldZ = (pointCharge.ChargeValue * pointCharge.ChargeMultiplier * dif.z) / Mathf.Pow((float)separationR, 3);
+
+            eFieldX *= (1 / (4 * Mathf.PI * permittivity));
+            eFieldY *= (1 / (4 * Mathf.PI * permittivity));
+            eFieldZ *= (1 / (4 * Mathf.PI * permittivity));
+
+            electricField += new Vector3((float)eFieldX, (float)eFieldY, (float)eFieldZ);
+        }
+
+        return electricField;
+    }
+
     public void CloseMenu(){
         EFieldValuesMenuUI.SetActive(false);
     }
 
     public void ToggleVectorField() {
-    // Check if the toggle is checked (toggled on)
+        // Check if the toggle is checked (toggled on)
         if (toggleVectorField.isOn) {
             // Find minimum and maximum coordinates
-            Vector3 minCoordinates = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-            Vector3 maxCoordinates = new Vector3(float.MinValue, float.MinValue, float.MinValue);
-
-            foreach (PointCharge pointCharge in FindObjectsOfType<PointCharge>()) {
-                Vector3 position = pointCharge.transform.position;
-                minCoordinates = Vector3.Min(minCoordinates, position);
-                maxCoordinates = Vector3.Max(maxCoordinates, position);
-            }
+            CalculateBoundingBox();
 
             // Calculate center point
-            Vector3 centerPoint = (minCoordinates + maxCoordinates) / 2f;
+            Vector3 centerPoint = (minBoundingBox + maxBoundingBox) / 2f;
+
+            // Calculate average position of test charges
+            Vector3 averageNonTestChargePosition = Vector3.zero;
+            int count = 0;
+            foreach (PointCharge pointCharge in FindObjectsOfType<PointCharge>()) {
+                if (pointCharge.ChargeValue != 0) {
+                    averageNonTestChargePosition += pointCharge.transform.position;
+                    count++;
+                }
+            }
+            averageNonTestChargePosition /= count;
+
+            // Offset center point by the average position of test charges
+            centerPoint += averageNonTestChargePosition;
 
             // Calculate dimensions
-            Vector3 dimensions = maxCoordinates - minCoordinates;
+            Vector3 dimensions = maxBoundingBox - minBoundingBox;
             float maxDimension = Mathf.Max(dimensions.x, dimensions.y, dimensions.z);
-            Vector3 cubeDimensions = new Vector3(maxDimension+10, maxDimension+10, maxDimension+10);
+            maxDimension += 5;
 
-            // Create bounding box cube
-            boundingBoxCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            boundingBoxCube.name = "BoundingBoxCube"; // Set the name
-            boundingBoxCube.transform.position = centerPoint;
-            boundingBoxCube.transform.localScale = cubeDimensions;
+            // I am skeptical of this...
+            for (float x = centerPoint.x - maxDimension / 2; x <= centerPoint.x + maxDimension / 2; x += fieldDensity) {
+                for (float y = centerPoint.y - maxDimension / 2; y <= centerPoint.y + maxDimension / 2; y += fieldDensity) {
+                    for (float z = centerPoint.z - maxDimension / 2; z <= centerPoint.z + maxDimension / 2; z += fieldDensity) {
+                        Vector3 position = new Vector3(x, y, z);
+                        Vector3 electricField = CalculateElectricFieldAtPoint(position);
+                        InstantiateVectorPrefab(position, electricField);
+                    }
+                }
+            }
+
+            
+            
+            
         } else {
             // If the toggle is unchecked (toggled off), destroy the bounding box cube
-            Destroy(boundingBoxCube);
+            foreach (GameObject vector in instantiatedVectors) {
+                Destroy(vector);
+            }
+            instantiatedVectors.Clear(); // Clear the list
         }
-    }       
+    }
+
+    /*
+    This function calculates the particular region where I want to render the vector
+    field.
+    */
+    void CalculateBoundingBox() {
+        minBoundingBox = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+        maxBoundingBox = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+        foreach (PointCharge pointCharge in FindObjectsOfType<PointCharge>()) {
+            Vector3 position = pointCharge.transform.position;
+            minBoundingBox = Vector3.Min(minBoundingBox, position);
+            maxBoundingBox = Vector3.Max(maxBoundingBox, position);
+        }
+    }
+
+    /*
+    This function uses the electric field vector prefab and instantiates it and rotates it
+    to the proper rotation.
+    */
+    void InstantiateVectorPrefab(Vector3 position, Vector3 electricField) {
+        // Instantiate the vector prefab at the given position
+        GameObject vectorPrefabInstance = Instantiate(fieldVector, position, Quaternion.identity);
+
+        // Calculate rotation to align the vector with the electric field direction
+        Quaternion rotation = Quaternion.LookRotation(electricField.normalized);
+
+        // Set the rotation of the instantiated prefab
+        vectorPrefabInstance.transform.rotation = rotation;
+
+        // Add the instantiated vector to the list
+        instantiatedVectors.Add(vectorPrefabInstance);
+    }
 }
